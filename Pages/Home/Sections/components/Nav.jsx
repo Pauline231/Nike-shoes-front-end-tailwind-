534f8f20ed5eacc084bf4afd2ec7a723deef0b15
pawan <pawantamang@Pawans-MacBook-Pro.local>
2025-10-29 15:50:23 +0545

    logo fix

--- Pages/Home/Sections/components/Nav.jsx
+++ Pages/Home/Sections/components/Nav.jsx
@@ -23,7 +23,7 @@ useEffect(()=>{
   return (
     <header className='padding-x py-8 absolute z-10 w-full'>
       <nav className='flex justify-between items-center max-container'>
-        <a href="\"><img src='../src/assets/images/header-logo.svg' 
+        <a href="\"><img src='../../../../src/assets/images/header-logo.svg' 
         alt='LOGO' width={130} height={29}/></a>
         <ul className='flex-1 flex justify-end items-center gap-16
         max-lg:hidden'>
